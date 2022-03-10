e3778a6f73b191fb2ff87fc681882f53b2af9878
piotrst99 <piotrstukus@outlook.com>
2022-03-10 11:18:53 +0100

    MainMenu

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent {
+  
+  public boardSizes: string[] = ['5 x 5', '6 x 6'];
+  public selectedSize: string = '';
+  //public index: number = 0;
+
+  public showLevels():void{
+    
+  }
+
+  selectBoardSize(event: any): void{
+    //console.log(this.selectedSize);
+    
+  }
 
   constructor() { }
 
